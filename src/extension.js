254bef9d9511b02fe1e9d533c33ed53197c9a55b
Alex Layton <alex@layton.in>
2020-02-20 17:28:58 -0500

    Register with coc

--- src/extension.js
+++ src/extension.js
@@ -1,11 +1,11 @@
-const { languages } = require('coc.nvim')
+const {languages, commands, workspace} = require('coc.nvim')
 
 const EditProvider = require('./edit-provider')
-const { supportedLanguageIds } = require('./language-map')
+const {supportedLanguageIds} = require('./language-map')
 
 const langs = [
-  ...supportedLanguageIds.map(language => ({ scheme: 'file', language })),
-  ...supportedLanguageIds.map(language => ({ scheme: 'untitled', language }))
+  ...supportedLanguageIds.map(language => ({scheme: 'file', language})),
+  ...supportedLanguageIds.map(language => ({scheme: 'untitled', language}))
 ]
 
 exports.activate = function (context) {
@@ -15,6 +15,38 @@ exports.activate = function (context) {
   )
   context.subscriptions.push(
     languages.registerDocumentFormattingEditProvider(langs, editProvider)
+  )
+
+  context.subscriptions.push(
+    commands.registerCommand('prettier-standard.formatFile', async () => {
+      let document = await workspace.document
+      let prettierConfig = workspace.getConfiguration('prettier', document.uri)
+      let onlyUseLocalVersion = prettierConfig.get(
+        'onlyUseLocalVersion',
+        false
+      )
+      if (
+        onlyUseLocalVersion &&
+        (!hasLocalPrettierInstalled(Uri.parse(document.uri).fsPath) ||
+          document.schema != 'file')
+      ) {
+        workspace.showMessage(
+          `Flag prettier.onlyUseLocalVersion is set, but prettier is not installed locally. No operation will be made.`,
+          'warning'
+        )
+        return
+      }
+      let edits = await format(
+        document.content,
+        document.textDocument,
+        {}
+      ).then(code => [
+        TextEdit.replace(fullDocumentRange(document.textDocument), code),
+      ])
+      if (edits && edits.length) {
+        await document.applyEdits(workspace.nvim, edits)
+      }
+    })
   )
 }
 
